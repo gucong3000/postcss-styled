f1d7efce7912c2699d2e195048d726d45b0206f6
刘祺 <gucong@gmail.com>
2018-06-19 18:27:35 +0800

    Add `document.lang` (#26)

--- test/styled.js
+++ test/styled.js
@@ -8,18 +8,19 @@ describe("styled-components", () => {
 		const file = require.resolve("./fixtures/styled");
 		let code = fs.readFileSync(file);
 
-		const root = syntax.parse(code, {
+		const document = syntax.parse(code, {
 			from: file,
 		});
 
 		code = code.toString();
-		expect(root.toString(), code);
+		expect(document.toString(), code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
 
-		expect(root.nodes).to.have.lengthOf(1);
-		expect(root.first.nodes).to.have.lengthOf(8);
+		expect(document.nodes).to.have.lengthOf(1);
+		expect(document.first.nodes).to.have.lengthOf(8);
 
 		const lines = code.match(/^.+$/gm).slice(3).map(line => (line.replace(/^\s*(.+?);?\s*$/, "$1")));
-		root.first.nodes.forEach((decl, i) => {
+		document.first.nodes.forEach((decl, i) => {
 			if (i) {
 				expect(decl).to.have.property("type", "decl");
 			} else {
@@ -37,70 +38,76 @@ describe("styled-components", () => {
 			"}",
 			"",
 		].join("\n");
-		const root = syntax.parse(code, {
+		const document = syntax.parse(code, {
 			from: "empty_template_literal.js",
 		});
-		expect(root.toString()).to.equal(code);
-		expect(root.nodes).to.have.lengthOf(0);
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+		expect(document.nodes).to.have.lengthOf(0);
 	});
 
 	it("skip javascript syntax error", () => {
 		const code = "\\`";
-		const root = syntax.parse(code, {
+		const document = syntax.parse(code, {
 			from: "syntax_error.js",
 		});
-		expect(root.toString()).to.equal(code);
-		expect(root.nodes).to.have.lengthOf(0);
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+		expect(document.nodes).to.have.lengthOf(0);
 	});
 
 	it("illegal template literal", () => {
 		const code = "`$\n{display: block}\n${g} {}`";
-		const root = syntax.parse(code, {
+		const document = syntax.parse(code, {
 			from: "illegal_template_literal.js",
 		});
-		expect(root.toString()).to.equal(code);
-		expect(root.nodes).to.have.lengthOf(1);
-		expect(root.first.nodes).to.have.lengthOf(2);
-		expect(root.first.first).have.property("type", "rule");
-		expect(root.first.first).have.property("selector", "$");
-		expect(root.last.last).have.property("type", "rule");
-		expect(root.last.last).have.property("selector", "${g}");
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+		expect(document.nodes).to.have.lengthOf(1);
+		expect(document.first.nodes).to.have.lengthOf(2);
+		expect(document.first.first).have.property("type", "rule");
+		expect(document.first.first).have.property("selector", "$");
+		expect(document.last.last).have.property("type", "rule");
+		expect(document.last.last).have.property("selector", "${g}");
 	});
 
 	it("skip CSS syntax error", () => {
 		const code = "`a{`";
-		const root = syntax.parse(code, {
+		const document = syntax.parse(code, {
 			from: "css_syntax_error.js",
 		});
-		expect(root.toString()).to.equal(code);
-		expect(root.nodes).to.have.lengthOf(0);
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+		expect(document.nodes).to.have.lengthOf(0);
 	});
 
 	it("fix CSS syntax error", () => {
 		const code = "`a{`";
-		const root = syntax({
+		const document = syntax({
 			css: "safe-parser",
 		}).parse(code, {
 			from: "postcss-safe-parser.js",
 		});
-		expect(root.toString()).to.equal("`a{}`");
-		expect(root.nodes).to.have.lengthOf(1);
-		expect(root.first.nodes).to.have.lengthOf(1);
-		expect(root.first.first).have.property("type", "rule");
-		expect(root.first.first).have.property("selector", "a");
+		expect(document.toString()).to.equal("`a{}`");
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+		expect(document.nodes).to.have.lengthOf(1);
+		expect(document.first.nodes).to.have.lengthOf(1);
+		expect(document.first.first).have.property("type", "rule");
+		expect(document.first.first).have.property("selector", "a");
 	});
 
 	it("fix styled syntax error", () => {
 		const code = "`${ a } {`";
-		const root = syntax({
+		const document = syntax({
 			css: "safe-parser",
 		}).parse(code, {
 			from: "styled-safe-parse.js",
 		});
-		expect(root.toString()).to.equal("`${ a } {}`");
-		expect(root.nodes).to.have.lengthOf(1);
-		expect(root.first.nodes).to.have.lengthOf(1);
-		expect(root.first.first).have.property("type", "rule");
-		expect(root.first.first).have.property("selector", "${ a }");
+		expect(document.toString()).to.equal("`${ a } {}`");
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+		expect(document.nodes).to.have.lengthOf(1);
+		expect(document.first.nodes).to.have.lengthOf(1);
+		expect(document.first.first).have.property("type", "rule");
+		expect(document.first.first).have.property("selector", "${ a }");
 	});
 });
